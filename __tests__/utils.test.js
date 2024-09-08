73247e9f50ce11203ebf57187e8b3fb12eddea3a
Fary~ <65035293+fary58@users.noreply.github.com>
2024-09-08 23:42:44 +0100

    double quotes removed

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -6,53 +6,53 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-"use strict";
+'use strict';
 
-const { ServerSideUtils } = require("facebook-nodejs-business-sdk");
-const sha256 = require("js-sha256");
+const { ServerSideUtils } = require('facebook-nodejs-business-sdk');
+const sha256 = require('js-sha256');
 
-describe("ServerSideUtils", () => {
-  describe("normalizeAndHash", () => {
-    test("normalizes and hashes the input", () => {
-      const value = "  Eg  ";
-      const expected = sha256("eg");
-      const actual = ServerSideUtils.normalizeAndHash(value, "ln");
+describe('ServerSideUtils', () => {
+  describe('normalizeAndHash', () => {
+    test('normalizes and hashes the input', () => {
+      const value = '  Eg  ';
+      const expected = sha256('eg');
+      const actual = ServerSideUtils.normalizeAndHash(value, 'ln');
 
       expect(actual).toBe(expected);
     });
 
-    test("does not double hash sha256", () => {
-      const value = sha256("11234567890");
-      const actual = ServerSideUtils.normalizeAndHash(value, "ph");
+    test('does not double hash sha256', () => {
+      const value = sha256('11234567890');
+      const actual = ServerSideUtils.normalizeAndHash(value, 'ph');
 
       expect(actual).toBe(value);
     });
 
-    test("does not double hash md5", () => {
-      const value = "2a6a84e9e44441afbd75cc19ce28be37";
-      const actual = ServerSideUtils.normalizeAndHash(value, "ln");
+    test('does not double hash md5', () => {
+      const value = '2a6a84e9e44441afbd75cc19ce28be37';
+      const actual = ServerSideUtils.normalizeAndHash(value, 'ln');
 
       expect(actual).toBe(value);
     });
 
-    test("validates the input", () => {
+    test('validates the input', () => {
       const normalizeAndHash = () => {
-        ServerSideUtils.normalizeAndHash("1234", "em");
+        ServerSideUtils.normalizeAndHash('1234', 'em');
       };
       expect(normalizeAndHash).toThrow();
     });
-    test("handles empty string correctly", () => {
-      const value = "";
+    test('handles empty string correctly', () => {
+      const value = '';
       const expected = sha256(value);
-      const actual = ServerSideUtils.normalizeAndHash(value, "ln");
+      const actual = ServerSideUtils.normalizeAndHash(value, 'ln');
 
       expect(actual).toBe(expected);
     });
   });
 
-  describe("toSHA256", () => {
-    test("hashes the value", () => {
-      const value = "test123";
+  describe('toSHA256', () => {
+    test('hashes the value', () => {
+      const value = 'test123';
       const expected = sha256(value);
 
       expect(ServerSideUtils.toSHA256(value)).toBe(expected);
